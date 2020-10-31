c9508aed0a3a4fc1abeee57917637034b7ebd3bf
amine.bennani <amine.bennani@octo.com>
2020-10-31 14:42:43 +0100

    Initialize infra with vpc and ec2 cluster

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_public_subnets" {
+  description = "IDs of the VPC's public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "ec2_instance_public_ips" {
+  description = "Public IP adresses of EC2 instance"
+  value       = module.ec2_instances.public_ip
+}
